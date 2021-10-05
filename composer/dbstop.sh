32dc4f3a703b47cad6e00f4fbbdd864a4b636069
qtn3 <qtn3@njit.edu>
2021-10-05 22:55:08 +0000

    composer scripts

--- composer/dbstop.sh
+++ composer/dbstop.sh
@@ -2,9 +2,9 @@
 
 if systemctl status mysql | grep "active" > /dev/null
 then
-    echo Starting MySQL DB..
+    echo Stopping MySQL DB..
     sudo systemctl stop mysql
-    echo MySQL DB Status: Active!
+    echo MySQL DB Status: Inactive!
 else
     echo It\'s already down!
 fi
